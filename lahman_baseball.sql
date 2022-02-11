1554a4ff9622532f2e698e1fdc09b816d2a2d0be
jacob-parks98 <jacobnparks98@gmail.com>
2022-02-10 21:02:42 -0600

    Working on number 8, with help from group

--- lahman_baseball.sql
+++ lahman_baseball.sql
@@ -18,6 +18,19 @@ WHERE schoolname = 'Vanderbilt University'
 GROUP BY name, schoolname
 ORDER BY total_salary DESC;
 
+--Josh
+SELECT namefirst || ' ' || namelast AS name, schoolname, SUM(salary) AS total_salary
+FROM people
+INNER JOIN collegeplaying AS cp
+USING (playerid)
+INNER JOIN schools
+USING (schoolid)
+INNER JOIN salaries
+USING (playerid)
+WHERE schoolname = 'Vanderbilt University'
+GROUP BY name, schoolname
+ORDER BY total_salary DESC;
+
 --David Price, deservedly, has made the highest total salary
 
 /* 2. Using the fielding table, group players into three groups based on their position: label players with 
@@ -76,7 +89,7 @@ FROM batting
 INNER JOIN people
 USING (playerid)
 WHERE yearid = 2016
-AND sb + cs > 20
+AND sb + cs >= 20
 ORDER BY success_percentage DESC;
 	
 /* Chris Owings had the most success stealing bases, but in my humble opinion, Billy Hamilton was far 
@@ -100,7 +113,7 @@ AND wswin = 'N';
 /* The largest number of wins by a team that did not win it all was 116, done twice, once by the cubs and once 
 by the mariners */
 
-SELECT name,w
+SELECT name,w, yearid
 FROM teams
 WHERE w = 
 (SELECT MIN(w)
@@ -109,10 +122,205 @@ WHERE yearid BETWEEN 1970 AND 2016
 AND wswin = 'Y')
 AND wswin = 'Y';
 
-/* The smallest number of wins for a world series winner was 63, done by the Dodgers */
+/* The smallest number of wins for a world series winner was 63, done by the Dodgers in the 1981 season
+in which there was a players strike, much like this season. */
 
-
-
-SELECT MAX
+SELECT name,w, yearid
 FROM teams
-WHERE yearid = 2016;
+WHERE w = 
+(SELECT MIN(w)
+FROM teams
+WHERE yearid BETWEEN 1970 AND 2016
+ AND yearid != 1981
+AND wswin = 'Y')
+AND wswin = 'Y';
+
+--excluding 1981, the minimum number of games won by a world series winner was 83.
+
+WITH wins AS (
+	SELECT yearid, w
+	FROM teams 
+	WHERE wswin = 'Y'
+	AND yearid BETWEEN 1970 AND 2016
+),
+max_wins AS (
+	SELECT yearid, MAX(w) AS max_w
+	FROM teams
+	WHERE yearid BETWEEN 1970 AND 2016
+	GROUP BY yearid
+)
+SELECT 
+	(SELECT COUNT(*)
+	FROM wins
+	INNER JOIN max_wins
+	USING (yearid)
+	WHERE w = max_w) AS years, 
+	(SELECT COUNT(*)
+	FROM wins
+	INNER JOIN max_wins
+	USING (yearid)
+	WHERE w = max_w) *100.0 / (SELECT COUNT(*)
+	FROM wins) AS percentage
+
+
+/* There were 12 years where the team that won the most regular season also won the world series from 1970 to 2016. This was
+26% of the time. */
+
+/* 6. Which managers have won the TSN Manager of the Year award in both the National League (NL) and the American 
+League (AL)? Give their full name and the teams that they were managing when they won the award. */
+--COME BACK
+
+WITH two_league_winners AS (
+	SELECT 
+		namefirst || ' ' || namelast AS name,
+		COUNT(DISTINCT lgid) AS al_nL_count
+	FROM awardsmanagers
+	INNER JOIN people
+	USING (playerid)
+	WHERE awardid = 'TSN Manager of the Year'
+	AND lgid IN ('AL', 'NL')
+	GROUP BY name
+	HAVING COUNT(DISTINCT lgid) >= 2)
+SELECT DISTINCT 
+	namefirst || ' ' || namelast AS name,
+	t.name AS team_name,
+	am.lgid, 
+	am.yearid
+FROM awardsmanagers AS am
+INNER JOIN people
+USING (playerid)
+INNER JOIN managers
+USING (playerid,yearid)
+INNER JOIN teams AS t
+USING(teamid, yearid)
+WHERE awardid = 'TSN Manager of the Year'
+AND namefirst || ' ' || namelast IN (SELECT name FROM two_league_winners)
+ORDER BY name;
+
+
+
+/*7. Which pitcher was the least efficient in 2016 in terms of salary / strikeouts? Only consider pitchers who started at
+least 10 games (across all teams). Note that pitchers often play for more than one team in a season,
+so be sure that you are counting all stats for each player. */
+
+SELECT namefirst || ' ' || namelast AS name, MAX(salary) / SUM(so) AS salary_per_SO
+FROM pitching as p1
+INNER JOIN people AS p2
+USING (playerid)
+INNER JOIN salaries
+USING (playerid, yearid)
+WHERE p1.yearid = 2016
+GROUP BY name
+HAVING SUM(gs) >= 10
+ORDER BY salary_per_SO;
+
+--Chris
+WITH strikeouts AS (
+	SELECT 
+		playerid, 
+		SUM(so) AS year_strikeouts
+	FROM pitching
+	WHERE yearid = 2016
+	GROUP BY playerid
+	HAVING SUM(gs) >= 10
+)
+
+SELECT
+	namefirst,
+	namelast,
+	TO_CHAR(ROUND(salary::numeric/year_strikeouts,2),'l999,999,999D99') AS efficiency
+FROM strikeouts
+INNER JOIN people
+USING (playerid)
+INNER JOIN salaries
+USING (playerid)
+WHERE salaries.yearid = 2016
+ORDER BY efficiency DESC;
+
+
+--Robbie Ray was the most efficient pitcher in salary per strikeout in 2016
+
+/* 8. Find all players who have had at least 3000 career hits. Report those players' names, total number of hits, and the 
+year they were inducted into the hall of fame (If they were not inducted into the hall of fame, put a null in that column.)
+Note that a player being inducted into the hall of fame is indicated by a 'Y' in the **inducted** column of the halloffame
+table. */
+
+WITH threek_hits AS (SELECT 
+	namefirst || ' ' || namelast AS name, 
+	SUM(h)
+FROM batting
+INNER JOIN people
+USING(playerid)
+GROUP BY name
+HAVING SUM(h) >=3000
+ORDER BY name),
+unique_hof_y AS (
+SELECT DISTINCT playerid, inducted, yearid
+FROM halloffame
+WHERE inducted = 'Y'
+)
+
+SELECT namefirst || ' ' || namelast AS name, 
+yearid
+FROM people
+LEFT JOIN unique_hof_y
+USING (playerid)
+WHERE namefirst || ' ' || namelast IN (SELECT name FROM threek_hits)
+
+--Chris
+WITH career_hits AS (
+	SELECT
+		playerid,
+		SUM(h) AS career_hits
+	FROM batting
+	GROUP BY playerid
+	HAVING SUM(h) >= 3000
+)
+
+SELECT
+	DISTINCT namefirst || ' ' || namelast AS name,
+	career_hits,
+	CASE WHEN inducted = 'Y' THEN hof.yearid
+	ELSE NULL
+	END AS hall_of_fame_induction
+FROM career_hits
+INNER JOIN people
+USING (playerid)
+INNER JOIN halloffame AS hof
+USING (playerid)
+ORDER BY name;
+
+--Josh
+WITH hof AS (
+	SELECT *
+	FROM halloffame
+	WHERE inducted = 'Y'
+)
+SELECT
+	namefirst,
+	namelast,
+	SUM(h),
+	hof.yearid
+FROM batting
+INNER JOIN people
+	USING (playerid)
+LEFT JOIN hof
+	USING (playerid)
+GROUP BY playerid, namefirst, namelast, hof.yearid
+HAVING sum(h) >= 3000
+ORDER BY namefirst;
+
+SELECT 
+	namefirst || ' ' || namelast AS name, 
+	SUM(h)
+FROM batting
+INNER JOIN people
+USING(playerid)
+GROUP BY name
+HAVING SUM(h) >=3000
+ORDER BY name
+
+
+
+
+/* 9. Find all players who had at least 1,000 hits for two different teams. Report those players' full names. */
